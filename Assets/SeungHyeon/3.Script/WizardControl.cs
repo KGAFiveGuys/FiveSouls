725d74259896f62a32a8b0e5a7d2bcf0bf21c177
JGoSeungHyeon <rhtmdgus4190@naver.com>
2023-10-31 19:02:47 +0900

    [update]Add Change Wizard Pattern Aura
    -위자드 플레이어와 거리에 따라 패턴 아우라 변경
    [done]

--- Assets/SeungHyeon/3.Script/WizardControl.cs
+++ Assets/SeungHyeon/3.Script/WizardControl.cs
@@ -25,7 +25,7 @@ public class Wizardinfo
 public class AttackEffect
 {
     public Effect effect;
-    public GameObject Effect_Obj;
+    public ParticleSystem Effect_Particle;
 }
 
 public class WizardControl : MonoBehaviour
@@ -33,9 +33,12 @@ public class WizardControl : MonoBehaviour
 
     private float Dist;//플레이어와 보스와의 거리
     private Animator Wizard_anim;
+    private float AttackTime = 0;
 
     [Header("이펙트")]
     [SerializeField] private AttackEffect[] Attack_effect;
+    [SerializeField] private ParticleSystem CurrnetEffect;
+    [SerializeField] private GameObject ReadyEffect;
 
     [Header("위자드 상태창")]
     [SerializeField] private Wizardinfo wizardinfo;
@@ -49,6 +52,16 @@ public class WizardControl : MonoBehaviour
     private void Update()
     {
         CheckPlayerPosition();
+        if(wizardinfo.status.Equals(Status.Ready))
+        {
+            AttackTime += Time.deltaTime;
+            Debug.Log(AttackTime);
+            if (AttackTime >= 5f)
+            {
+                Debug.Log("실행");
+               StartCoroutine(AttackReady(SelectPattern()));
+            }
+        }
     }
     public void CheckPlayerPosition()
     {
@@ -57,15 +70,27 @@ public class WizardControl : MonoBehaviour
         {
             Wizard_anim.SetBool("idle_combat", true);
             wizardinfo.status = Status.Ready;
-            Attack_effect[0].Effect_Obj.SetActive(true);
+            ReadyEffect.SetActive(true);
         }
+        
     }
-    public IEnumerator AttackReady()
+    public int SelectPattern()
     {
-        int randAttack = Random.Range(0, 2);
-        Attack_effect[randAttack].Effect_Obj.SetActive(true);
-        yield return new WaitForSeconds(2f);
-        //공격 코루틴 넣기
+        int rand = 0;
+        if (Dist <= 8f)
+        {
+            return rand;
+        }
+        rand = Random.Range(1, 3);
+        return rand;
     }
-
+    public IEnumerator AttackReady(int AttackPlayer)
+    {
+        CurrnetEffect = Attack_effect[AttackPlayer].Effect_Particle;
+        Attack_effect[AttackPlayer].Effect_Particle.Play();
+        AttackTime = 0;
+        yield return new WaitForSeconds(3f);
+        Wizard_anim.SetTrigger("Attack");
+        CurrnetEffect.Stop();
+    }
 }
