2d7da73b86c490508e46d04f5480973a88123d99
Woo-ChanHyeok <148195213+Woo-ChanHyeok@users.noreply.github.com>
2023-10-31 15:28:15 +0900

    [init] CreateUnityChanModel
    에셋 추가 후 모델 생성 및 걷기 달리기 모션 추가
    [done]

---
+++ Assets/WooChan/3.Script/UnityChanController.cs
@@ -0,0 +1,363 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(Animator))]
+public class UnityChanController : MonoBehaviour
+{
+    [field:Header("State")]
+    [field:SerializeField] public ControlState ControlState { get; set; } = ControlState.Controllable;
+
+    [Header("Input Actions")]
+    public InputAction move;
+    public InputAction rotate;
+    public InputAction weakAttack;
+    public InputAction strongAttack;
+    public InputAction block;
+    public InputAction jump;
+    public InputAction ragdollTest;
+
+    [Header("Movements")]
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float rotateSpeed;
+    [SerializeField] private float jumpForce;
+
+    //[Header("Equiments")]
+    //[SerializeField] private Transform swordParent;
+    //[SerializeField] private Transform sword;
+    
+    //[SerializeField] private Transform shieldParent;
+    //[SerializeField] private Transform shield;
+    
+    //[SerializeField] private float dropDelay = .5f;
+
+    //[SerializeField] private Vector3 swordOriginPos;
+    //[SerializeField] private Quaternion swordOriginRot;
+
+    //[SerializeField] private Vector3 shieldOriginPos;
+    //[SerializeField] private Quaternion shieldOriginRot;
+
+    private Rigidbody playerRigidbody;
+    private Animator playerAnimator;
+    #region AnimatorParameters
+    private readonly int moveX_hash = Animator.StringToHash("moveX");
+    private readonly int moveY_hash = Animator.StringToHash("moveY");
+    private readonly int isWeakAttack_hash = Animator.StringToHash("isWeakAttack");
+    private readonly int isStrongAttack_hash = Animator.StringToHash("isStrongAttack");
+    private readonly int isJump_hash = Animator.StringToHash("isJump");
+    private readonly int isBlock_hash = Animator.StringToHash("isBlock");
+    #endregion
+    #region Cached colliders & rigidbodies
+    [SerializeField] private List<Collider> ragdollColliders = new List<Collider>();
+    [SerializeField] private List<Rigidbody> ragdollRigidbodies = new List<Rigidbody>();
+    //[SerializeField] private List<Collider> weaponColliders = new List<Collider>();
+    //[SerializeField] private List<Rigidbody> weaponRigidbodies = new List<Rigidbody>();
+    //[SerializeField] private List<Collider> shieldColliders = new List<Collider>();
+    //[SerializeField] private List<Rigidbody> shieldRigidbodies = new List<Rigidbody>();
+    #endregion
+
+    private void Awake()
+    {
+        TryGetComponent(out playerRigidbody);
+        TryGetComponent(out playerAnimator);
+
+        //swordOriginPos = sword.localPosition;
+        //swordOriginRot = sword.localRotation;
+
+        //shieldOriginPos = sword.localPosition;
+        //shieldOriginRot = sword.localRotation;
+    }
+
+    private void Start()
+    {
+        //#region Set weapon colliders & rigidbodies
+        //foreach (Collider c in weaponColliders)
+        //{
+        //    c.enabled = false;
+        //}
+        //foreach (Rigidbody rb in weaponRigidbodies)
+        //{
+        //    rb.isKinematic = true;
+        //}
+        //#endregion
+        //#region Set shield colliders & rigidbodies
+        //foreach (Collider c in shieldColliders)
+        //{
+        //    c.enabled = false;
+        //}
+        //foreach (Rigidbody rb in shieldRigidbodies)
+        //{
+        //    rb.isKinematic = true;
+        //}
+        //#endregion
+    }
+
+    private void Update()
+    {
+        Move();
+        Rotate();
+    }
+
+    private void Move()
+    {
+        if (ControlState.Equals(ControlState.Uncontrollable))
+            return;
+        
+        transform.Translate(
+            desiredMove.x * moveSpeed * Time.deltaTime,
+            0,
+            desiredMove.y * moveSpeed * Time.deltaTime
+        );
+        playerAnimator.SetFloat(moveX_hash, desiredMove.x);
+        playerAnimator.SetFloat(moveY_hash, desiredMove.y);
+    }
+
+    private void Rotate()
+    {
+        if (ControlState.Equals(ControlState.Uncontrollable))
+            return;
+
+        transform.Rotate(Vector3.up * desiredRotate.x * rotateSpeed * Time.deltaTime);
+    }
+
+    private void OnEnable()
+    {
+        #region Enable InputActions
+        move.performed += OnMovePerformed;
+        move.canceled += OnMoveCanceled;
+        move.Enable();
+
+        rotate.performed += OnRotatePerformed;
+        rotate.canceled += OnRotateCanceled;
+        rotate.Enable();
+
+        weakAttack.performed += OnWeakAttackPerformed;
+        weakAttack.canceled += OnWeakAttackCanceled;
+        weakAttack.Enable();
+
+        strongAttack.performed += OnStrongAttackPerformed;
+        strongAttack.canceled += OnStrongAttackCanceled;
+        strongAttack.Enable();
+
+        block.performed += OnBlockPerformed;
+        block.canceled += OnBlockCanceled;
+        block.Enable();
+
+        jump.performed += OnJumpPerformed;
+        jump.canceled += OnJumpCanceled;
+        jump.Enable();
+
+        ragdollTest.performed += OnRagdollPerformed;
+        ragdollTest.canceled += OnRagdollCanceled;
+        ragdollTest.Enable();
+        #endregion
+    }
+
+    private void OnDisable()
+    {
+        #region Disable InputActions
+        move.Disable();
+        move.performed -= OnMovePerformed;
+        move.performed -= OnMoveCanceled;
+
+        rotate.Disable();
+        rotate.performed -= OnRotatePerformed;
+        rotate.performed -= OnRotateCanceled;
+
+        weakAttack.Disable();
+        weakAttack.performed -= OnWeakAttackPerformed;
+        weakAttack.canceled -= OnWeakAttackCanceled;
+
+        strongAttack.Disable();
+        strongAttack.performed -= OnStrongAttackPerformed;
+        strongAttack.canceled -= OnStrongAttackCanceled;
+
+        block.performed -= OnBlockPerformed;
+        block.canceled -= OnBlockCanceled;
+        block.Disable();
+
+        jump.performed -= OnJumpPerformed;
+        jump.canceled -= OnJumpCanceled;
+        jump.Disable();
+
+        ragdollTest.performed -= OnRagdollPerformed;
+        ragdollTest.canceled -= OnRagdollCanceled;
+        ragdollTest.Disable();
+        #endregion
+    }
+
+    private Vector2 desiredMove;
+    #region move_Action
+    private void OnMovePerformed(InputAction.CallbackContext context)
+    {
+        desiredMove = context.ReadValue<Vector2>();
+    }
+    private void OnMoveCanceled(InputAction.CallbackContext context)
+    {
+        desiredMove = Vector2.zero;
+    }
+    #endregion
+    private Vector2 desiredRotate;
+    #region rotate_Action
+    private void OnRotatePerformed(InputAction.CallbackContext context)
+    {
+        desiredRotate = context.ReadValue<Vector2>().normalized;
+    }
+    private void OnRotateCanceled(InputAction.CallbackContext context)
+    {
+        desiredRotate = Vector2.zero;
+    }
+    #endregion
+    #region jump_Action
+    private void OnJumpPerformed(InputAction.CallbackContext context)
+    {
+        var isJump = context.ReadValueAsButton();
+        if (isJump)
+            playerAnimator.SetBool(isJump_hash, true);
+    }
+    private void OnJumpCanceled(InputAction.CallbackContext context)
+    {
+        playerAnimator.SetBool(isJump_hash, false);
+    }
+    #endregion
+    #region block_Action
+    private void OnBlockPerformed(InputAction.CallbackContext context)
+    {
+        var isBlock = context.ReadValueAsButton();
+        if (isBlock)
+        {
+            ControlState = ControlState.Uncontrollable;
+            playerAnimator.SetBool(isBlock_hash, true);
+        }
+    }
+    private void OnBlockCanceled(InputAction.CallbackContext context)
+    {
+        ControlState = ControlState.Controllable;
+        playerAnimator.SetBool(isBlock_hash, false);
+    }
+    #endregion
+    #region weakAttack_Action
+    private void OnWeakAttackPerformed(InputAction.CallbackContext context)
+    {
+        var isWeakAttack = context.ReadValueAsButton();
+        if (isWeakAttack)
+        {
+            ControlState = ControlState.Uncontrollable;
+            playerAnimator.SetBool(isWeakAttack_hash, true);
+        }
+    }
+    private void OnWeakAttackCanceled(InputAction.CallbackContext context)
+    {
+        ControlState = ControlState.Controllable;
+        playerAnimator.SetBool(isWeakAttack_hash, false);
+    }
+    #endregion
+    #region strongAttack_Action
+    private void OnStrongAttackPerformed(InputAction.CallbackContext context)
+    {
+        var isStrongAttack = context.ReadValueAsButton();
+        if (isStrongAttack)
+        {
+            ControlState = ControlState.Uncontrollable;
+            playerAnimator.SetTrigger(isStrongAttack_hash);
+        }
+    }
+    private void OnStrongAttackCanceled(InputAction.CallbackContext context)
+    {
+        ControlState = ControlState.Controllable;
+        playerAnimator.SetBool(isStrongAttack_hash, false);
+    }
+    #endregion
+    #region ragdoll_Action
+    private void OnRagdollPerformed(InputAction.CallbackContext context)
+    {
+        var isRagdoll = context.ReadValueAsButton();
+        if (isRagdoll)
+        {
+            ToggleRagdoll(true);
+            //StartCoroutine(HandleEquipment(true));
+        }
+    }
+    private void OnRagdollCanceled(InputAction.CallbackContext context)
+    {
+        ToggleRagdoll(false);
+        //StartCoroutine(HandleEquipment(false));
+    }
+    #endregion
+
+    public void ToggleRagdoll(bool isRagdoll)
+    {
+        #region Toggle colliders & rigidbodies
+        foreach (var c in ragdollColliders)
+        {
+            c.enabled = isRagdoll;
+        }
+        foreach (var rb in ragdollRigidbodies)
+        {
+            rb.useGravity = isRagdoll;
+            rb.isKinematic = !isRagdoll;
+        }
+        #endregion
+
+        // Toggle animation & control
+        playerAnimator.enabled = !isRagdoll;
+        ControlState = !isRagdoll ? ControlState.Controllable : ControlState.Uncontrollable;
+    }
+
+    ///// <summary>
+    ///// Drop or pick-up equipments (sword and shield)
+    ///// </summary>
+    ///// <param name="isDrop">
+    ///// <para>true if drop</para>
+    ///// <para>false if pick-up</para>
+    ///// </param>
+    //private IEnumerator HandleEquipment(bool isDrop)
+    //{
+    //    if (isDrop)
+    //        yield return new WaitForSeconds(dropDelay);
+        
+    //    #region Toggle weapon colliders & rigidbodies
+    //    foreach (var c in weaponColliders)
+    //    {
+    //        c.enabled = isDrop;
+    //    }
+    //    foreach (var rb in weaponRigidbodies)
+    //    {
+    //        rb.useGravity = isDrop;
+    //        rb.isKinematic = !isDrop;
+    //        if (isDrop)
+    //        {
+    //            rb.transform.SetParent(null);
+    //        }
+    //        else
+    //        {
+    //            rb.transform.SetParent(swordParent);
+    //            sword.localPosition = swordOriginPos;
+    //            sword.localRotation = swordOriginRot;
+    //        }
+    //    }
+    //    #endregion
+    //    #region Toggle shield colliders & rigidbodies
+    //    foreach (var c in shieldColliders)
+    //    {
+    //        c.enabled = isDrop;
+    //    }
+    //    foreach (var rb in shieldRigidbodies)
+    //    {
+    //        rb.useGravity = isDrop;
+    //        rb.isKinematic = !isDrop;
+    //        if (isDrop)
+    //        {
+    //            rb.transform.SetParent(null);
+    //        }
+    //        else
+    //        {
+    //            rb.transform.SetParent(shieldParent);
+    //            shield.localPosition = shieldOriginPos;
+    //            shield.localRotation = shieldOriginRot;
+    //        }
+    //    }
+    //    #endregion
+    //}
+}
