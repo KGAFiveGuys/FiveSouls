f62cbf40f69f8c06dd454991c189dd829e1781b5
Won9012 <chldnjsdy2@gmail.com>
2023-11-13 20:51:56 +0900

    [fix] merge conflict resolve
    -pull request 충돌 해결을 위해 , 재 작업 후 commit 작업.

---
+++ Assets/WonYong/3.Script/ReturnRock.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReturnRock : MonoBehaviour
+{
+    private GameObject rockPooling;
+    private void Awake()
+    {
+        rockPooling = GameObject.Find("RockPoolposition");
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.CompareTag("Rock"))
+        {
+            var offset = new Vector3(0f, 0f, Random.Range(-50f, 50f));
+            other.gameObject.SetActive(false);
+            RockPooling.Rock_count++;
+            other.gameObject.transform.position = rockPooling.transform.position + offset;
+        }
+    }
+}
