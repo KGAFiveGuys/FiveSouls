267a6088f0d3a47b69a89aa8867e7fd358245641
Won9012 <chldnjsdy2@gmail.com>
2023-11-13 17:51:21 +0900

    [update] object pooling && map add
    -object pooling을 이용하여 중간 통로에서 돌을 생성하여 Player이동을 방해
    [done]
    -map을 추가로 제작 (보스에게 도달하기 전 통로 제작)
    -boss trigger 제작 (타격시 보스 생성)

--- Assets/WonYong/3.Script/RockController.cs
+++ Assets/WonYong/3.Script/RockController.cs
@@ -6,8 +6,6 @@ public class RockController : MonoBehaviour
 {
     private void OnCollisionEnter(Collision collision)
     {
-        //삭제할 돌땡이
-
         if (collision.gameObject.CompareTag("Player"))
         {
             Destroy(gameObject, 0.5f);
---
+++ Assets/WonYong/3.Script/RockPooling.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockPooling : MonoBehaviour
+{
+    [SerializeField] private GameObject RockPrefab_Two;
+    [SerializeField] private Transform pool_position;
+    [SerializeField] private int count;
+    List<GameObject> poolRock = new List<GameObject>();
+
+    public static int Rock_count;
+    private void Awake()
+    {
+        Creat_Rock_Pool(count);
+        Rock_count = count;
+    }
+
+    private void Start()
+    {
+        StartCoroutine(Rock_Creat_co());
+    }
+    private void Update()
+    {
+
+    }
+
+    private void Creat_Rock_Pool(int count)
+    {
+        for(int i = 0; i <count; i++)
+        {
+            Creat_Rock();
+        }
+    }
+
+    private void Creat_Rock()
+    {
+        var offset = new Vector3(0f,0f,Random.Range(-34f,36f));
+        var newRock = Instantiate(RockPrefab_Two, pool_position.position+offset, Quaternion.identity);
+        newRock.SetActive(false);
+        poolRock.Add(newRock);
+    }
+
+    private IEnumerator Rock_Creat_co()
+    {
+        
+        foreach (var rock in poolRock)
+        {
+            if (rock != null && !rock.activeSelf)
+            {
+                int randomValue = Random.Range(1, 5);
+                rock.SetActive(true);
+                Rigidbody rb = rock.GetComponent<Rigidbody>();
+
+                if (rb != null)
+                {
+                    Vector3 forceDirection = new Vector3(-10.0f, -25f, 0);
+                    float forceMagnitude = 10f;
+                    rb.AddForce(forceDirection * forceMagnitude, ForceMode.Impulse);
+                }
+
+                Rock_count--;
+                yield return new WaitForSeconds(randomValue);
+
+                if (Rock_count == 20)
+                {
+                    StartCoroutine(Rock_Creat_co());
+                }
+            }
+        }
+
+    }
+
+
+
+
+}
