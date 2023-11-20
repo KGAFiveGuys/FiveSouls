416116bda31943b48e024200b49fb58b61644804
SangWoo <swp1207@gmail.com>
2023-11-20 14:35:33 +0900

    [Create] Boss intro
    보스 등장씬 만드는중
    공격알람 수정완료
    [in-progress]

--- Assets/SWP/3.Script/Combat/AttackAlarm.cs
+++ Assets/SWP/3.Script/Combat/AttackAlarm.cs
@@ -7,12 +7,13 @@ public class AttackAlarm : MonoBehaviour
 {
     [Header("알람설정")]
     [SerializeField] private GameObject AlarmUI;
-    [SerializeField] private Image AlarmColor;
-    [SerializeField] private float Timer;
-    [SerializeField] private int MultiNum;
+    [SerializeField] private ParticleSystem Circle;
+    [SerializeField] private ParticleSystem Smoke;
     private PlayerController playerController;
-    private float flowTime;
-    private float Acolor;
+    //[SerializeField] private Image AlarmColor;
+    //[SerializeField] private float Timer;
+    //[SerializeField] private int MultiNum;
+    //private float flowTime;
 
     public static AttackAlarm Instance = null;
     private void Awake()
@@ -32,21 +33,18 @@ public class AttackAlarm : MonoBehaviour
     private void Update()
     {
         ShowAlarm();
+        if (Input.GetKey(KeyCode.F3))
+        {
+            RedAlarm();
+        }
     }
 
     private void ShowAlarm()
     {
         if (playerController.LockedOnEnemy != null)
         {
-            var pos = Camera.main.WorldToScreenPoint(playerController.LockOnTargetPoint.transform.position);
-            var rectTransform = AlarmUI.GetComponent<RectTransform>();
-            var scale = rectTransform.localScale;
-            var width = rectTransform.rect.width;
-            var height = rectTransform.rect.height;
-            var xOffset = scale.x * (width / 2);
-            var yOffset = scale.y * (height / 2);
-
-            AlarmUI.transform.position = new Vector3(pos.x - xOffset, pos.y + yOffset, pos.z);
+            var pos = playerController.LockOnTargetPoint.transform.position;
+            AlarmUI.transform.position = new Vector3(pos.x, pos.y, pos.z);
         }
     }
 
@@ -61,50 +59,70 @@ public class AttackAlarm : MonoBehaviour
 
     private IEnumerator StrongAlarm()
     {
+        //if (playerController.LockedOnEnemy != null)
+        //{
+        //    AlarmUI.SetActive(true);
+        //    AlarmColor.color = new Color(255, 0, 0, Acolor);
+        //    var rectTransform = AlarmUI.GetComponent<RectTransform>();
+        //    var startScale = Vector3.one;
+        //    var endScale = Vector3.one * MultiNum;
+        //    flowTime = 0;
+        //    while (Timer > flowTime)
+        //    {
+        //        flowTime += Time.deltaTime;
+        //        rectTransform.localScale = Vector3.Lerp(startScale, endScale, flowTime / Timer);
+        //        AlarmColor.color = new Color(255, 0, 0, Acolor);
+
+        //        yield return null;
+        //    }
+        //    yield return null;
+        //    AlarmUI.SetActive(false);
+        //}
         if (playerController.LockedOnEnemy != null)
         {
-            Acolor = 255f;
-            AlarmUI.SetActive(true);
-            AlarmColor.color = new Color(255, 0, 0, Acolor);
-            var rectTransform = AlarmUI.GetComponent<RectTransform>();
-            var startScale = Vector3.one;
-            var endScale = Vector3.one * MultiNum;
-            flowTime = 0;
-            while (Timer > flowTime)
+            if (!Circle.isPlaying)
             {
-                flowTime += Time.deltaTime;
-                rectTransform.localScale = Vector3.Lerp(startScale, endScale, flowTime / Timer);
-                Acolor -= 40f;
-                AlarmColor.color = new Color(255, 0, 0, Acolor);
-
+                Circle.startColor = new Color32(125, 0, 0, 255);
+                Smoke.startColor = new Color32(255, 45, 45, 255);
                 yield return null;
+                Circle.Play();
+                Smoke.Play();
             }
-            yield return null;
-            AlarmUI.SetActive(false);
         }
     }
 
     private IEnumerator WeakAlarm()
     {
+        //if (playerController.LockedOnEnemy != null)
+        //{
+        //    Acolor = 255f;
+        //    AlarmUI.SetActive(true);
+        //    AlarmColor.color = new Color(255, 255, 0, Acolor);
+        //    var rectTransform = AlarmUI.GetComponent<RectTransform>();
+        //    var startScale = Vector3.one;
+        //    var endScale = Vector3.one * MultiNum;
+        //    flowTime = 0;
+        //    while (Timer > flowTime)
+        //    {
+        //        flowTime += Time.deltaTime;
+        //        rectTransform.localScale = Vector3.Lerp(startScale, endScale, flowTime / Timer);
+        //        Acolor -= 40f;
+        //        AlarmColor.color = new Color(255, 255, 0, Acolor);
+        //        yield return null;
+        //    }
+        //    yield return null;
+        //    AlarmUI.SetActive(false);
+        //}
         if (playerController.LockedOnEnemy != null)
         {
-            Acolor = 255f;
-            AlarmUI.SetActive(true);
-            AlarmColor.color = new Color(255, 255, 0, Acolor);
-            var rectTransform = AlarmUI.GetComponent<RectTransform>();
-            var startScale = Vector3.one;
-            var endScale = Vector3.one * MultiNum;
-            flowTime = 0;
-            while (Timer > flowTime)
+            if (!Circle.isPlaying)
             {
-                flowTime += Time.deltaTime;
-                rectTransform.localScale = Vector3.Lerp(startScale, endScale, flowTime / Timer);
-                Acolor -= 40f;
-                AlarmColor.color = new Color(255, 255, 0, Acolor);
+                Circle.startColor = new Color32(125, 125, 0, 255);
+                Smoke.startColor = new Color32(255, 255, 45, 255);
                 yield return null;
+                Circle.Play();
+                Smoke.Play();
             }
-            yield return null;
-            AlarmUI.SetActive(false);
         }
     }
 }
