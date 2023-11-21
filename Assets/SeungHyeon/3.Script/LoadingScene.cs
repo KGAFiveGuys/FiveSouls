805d7cfca0802ecc5ab502cecd5ba1788eebb259
JGoSeungHyeon <rhtmdgus4190@naver.com>
2023-11-21 13:44:57 +0900

    [update]IntroScene
    -인트로 씬 추가
    [done]
    -메인메뉴 제작중
    [in-progress]
    -FiveSouls폰트 추가

---
+++ Assets/SeungHyeon/3.Script/LoadingScene.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class LoadingScene : MonoBehaviour
+{
+    [SerializeField] Image image = null;
+    [SerializeField] TextMeshProUGUI text = null;
+
+    private void Start()
+    {
+        StartCoroutine(FadeTextToFullAlpha(1.5f, image, text));
+    }
+    public IEnumerator FadeTextToFullAlpha(float t, Image i, TextMeshProUGUI j)
+    {
+        i.color = new Color(i.color.r, i.color.g, i.color.b, 0);
+        j.color = new Color(j.color.r, j.color.g, j.color.b, 0);
+
+        while (i.color.a < 1.0f)
+        {
+            i.color = new Color(i.color.r, i.color.g, i.color.b, i.color.a + (Time.deltaTime / t));
+            yield return null;
+        }
+        i.color = new Color(i.color.r, i.color.g, i.color.b, 1);
+        while (i.color.a > 0.0f)
+        {
+            i.color = new Color(i.color.r, i.color.g, i.color.b, i.color.a - (Time.deltaTime / t));
+            yield return null;
+        }
+
+        while (j.color.a < 1.0f)
+        {
+            j.color = new Color(j.color.r, j.color.g, j.color.b, j.color.a + (Time.deltaTime / t));
+            yield return null;
+        }
+        j.color = new Color(j.color.r, j.color.g, j.color.b, 1);
+        while (j.color.a > 0.0f)
+        {
+            j.color = new Color(j.color.r, j.color.g, j.color.b, j.color.a - (Time.deltaTime / t));
+            yield return null;
+        }
+
+        SceneManager.LoadScene("Intro");
+    }
+}
