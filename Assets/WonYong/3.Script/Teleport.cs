4c73c15807c48e79103c48d5701e439a49056708
Won9012 <chldnjsdy2@gmail.com>
2023-11-23 12:35:28 +0900

    [update] 맵이동시 플레이어 위치 수정중
    진행중

--- Assets/WonYong/3.Script/Teleport.cs
+++ Assets/WonYong/3.Script/Teleport.cs
@@ -17,20 +17,9 @@ public class Teleport : MonoBehaviour
     {
         if (other.gameObject.layer == LayerMask.NameToLayer("Player"))
         {
-            StartCoroutine(TeleportPlayer_co());
+            playerController.gameObject.transform.position = positionToTeleport;
+            playerController.gameObject.transform.rotation = Quaternion.Euler(0, 90, 0);
         }
     }
 
-    private IEnumerator TeleportPlayer_co()
-    {
-        playerController.gameObject.GetComponent<Rigidbody>().useGravity = false;
-        playerController.gameObject.GetComponent<ConstantForce>().enabled = false;
-        playerController.gameObject.transform.position = positionToTeleport;
-        playerController.gameObject.transform.rotation = Quaternion.Euler(0, 90, 0);
-
-        yield return new WaitForSeconds(count);
-
-        playerController.gameObject.GetComponent<Rigidbody>().useGravity = true;
-        playerController.gameObject.GetComponent<ConstantForce>().enabled = true;
-    }
 }
